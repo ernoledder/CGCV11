26a95ccb07fd98bbe0cdd166709600c0be3c2e30
Véronne <veronners@live.nl>
2020-02-26 23:34:01 +0100

    block game

---
+++ BlockGame/Assets/CGCV11/BlockGame/Scripts/Ball.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace CGCV11.BlockGame.Scripts
+{
+    [RequireComponent(typeof(Rigidbody))]
+
+    public class Ball : MonoBehaviour
+    {
+        public GameManager gamemanager;
+        private Rigidbody _rigidBody;
+        [SerializeField] private float speed;
+
+        private void Awake()
+        {
+            _rigidBody = GetComponent<Rigidbody>();
+            _rigidBody.isKinematic = false;
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            if (other.gameObject.tag == "Water")
+            {
+                gamemanager.CollideWater(other);
+            }
+            if (other.gameObject.tag == "Brick")
+            {
+                gamemanager.CollideBrick(other);
+            }
+        }
+
+        public void Reset()
+        {
+            speed = 0;
+        }
+
+        private void Start()
+        {
+            _rigidBody.AddForce(new Vector3(0.001f, -speed, 0), ForceMode.Impulse);
+        }
+    }
+}
+
---
+++ BlockGame/Assets/CGCV11/BlockGame/Scripts/Paddle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CGCV11.BlockGame.Scripts
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class Paddle : MonoBehaviour
+    {
+        private Rigidbody _rigidBody;
+        [SerializeField] private float left, right, speed;
+        private Vector3 playerPos = new Vector3(0, 0.5f, 0);
+
+        private void Awake()
+        {
+            _rigidBody = GetComponent<Rigidbody>();
+            _rigidBody.isKinematic = true;
+        }
+
+        private void Reset()
+        {
+            left = -3;
+            right = 3;
+            speed = 12;
+        }
+
+        private void Update()
+        {
+            float step = speed * Time.deltaTime;
+            float xPos = transform.position.x + (Input.GetAxis("Horizontal") * step);
+            playerPos = new Vector3(Mathf.Clamp(xPos, -3, 3), 0.5f, 0);
+            transform.position = playerPos;
+        }
+    }
+}
+
